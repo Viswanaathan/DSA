141dbdb4f8f65f3148a5d6ae870934d43d12126d
Viswanaathan <Viswanaathanchidambaram@gmail.com>
2025-06-16 14:50:26 +0530

    Delete code directory

--- code/js/page.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("username").textContent = localStorage.getItem("Username");
-document.getElementById("logout").onclick = function() {
-    window.location.href = "login.html";
-    alert("Successfuly logged Out");
-};
